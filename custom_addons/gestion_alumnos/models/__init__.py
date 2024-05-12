646f6db0dc409f7b91b8c4b7140a2736f3ca6b27
juanjricci <jr.juanricci@gmail.com>
2024-05-12 12:34:23 -0300

    created the basic models

---
+++ custom_addons/gestion_alumnos/models/__init__.py
@@ -0,0 +1,1 @@
+from . import gestion_alumnos_models
---
+++ custom_addons/gestion_alumnos/models/gestion_alumnos_models.py
@@ -0,0 +1,32 @@
+from odoo import models, fields
+
+# Modelo Alumno
+class Alumno(models.Model):
+    _name = "gestion.alumnos.alumno"
+    _description = "alumno model"
+
+    nombre = fields.Char("Nombre", required=True)
+    apellido = fields.Char("Apellido", required=True)
+    fecha_de_nacimiento = fields.Date("Fecha de Nacimiento", required=True)
+    legajo = fields.Integer("N° de Legajo")
+    email = fields.Char("Email")
+    telefono = fields.Integer("Telefono")
+    direccion = fields.Char("Direccion")
+    pais = fields.Char("Pais")
+
+# Modelo Programa
+class Programa(models.Model):
+    _name = "gestion.alumnos.programa"
+    _description = "programa model"
+
+    nombre = fields.Char("Nombre", required=True)
+    descripcion = fields.Char("Descripcion", required=True)
+
+# Modelo Inscripción
+class Inscripcion(models.Model):
+    _name = 'gestion.alumnos.inscripcion'
+    _description = 'inscripcion model'
+
+    alumno_id = fields.Many2one('gestion_alumnos.alumno', "Alumno", required=True)
+    programa_id = fields.Many2one('gestion_alumnos.programa', "Programa", required=True)
+    fecha_inscripcion = fields.Date("Fecha de inscripcion", default=fields.Date.today)
