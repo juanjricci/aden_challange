536197ddde7c5b4cccbe4d0d00dd6dd4fad8cc23
juanjricci <jr.juanricci@gmail.com>
2024-05-12 12:00:19 -0300

    Created module gestion_alumnos

---
+++ custom_addons/gestion_alumnos/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Gestion de Alumnos",
+    'version': '1.0.0',
+    'depends': ['base'],
+    'author': "Juan Ricci",
+    'category': 'Administration',
+    'description': """
+    Este es un módulo de gestión de alumnos
+    """,
+    # data files always loaded at installation
+    'data': [
+        # 'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        # 'demo/demo_data.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
