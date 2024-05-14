441f109e61d5c99e013795ef42438abba1e069f3
juanjricci <jr.juanricci@gmail.com>
2024-05-14 10:29:34 -0300

    made the controller that resturns the list of inscripciones with the same program_id

--- custom_addons/gestion_alumnos/controllers/controllers.py
+++ custom_addons/gestion_alumnos/controllers/controllers.py
@@ -4,56 +4,42 @@ from odoo import http
 from odoo.http import Response, request
 
 class Tracking(http.Controller):
-    # @http.route('/dataset/<int:programa_id>', type='http', auth="public")
-    # def tracking(self, programa_id):
-    #     return "Hola mundo - " + str(programa_id)
+ 
+    # @http.route('/api/programas/<int:programa_id>', auth='public')
+    # def get_programas(self, programa_id):
+    #     # Obtener registros del modelo 'gestion.alumnos.programa'
+    #     programas = http.request.env['gestion.alumnos.programa'].search([
+    #         ('id', '=', programa_id)
+    #     ])
+
+    #     # Convertir registros a formato JSON
+    #     data = []
+    #     for programa in programas:
+    #         data.append({
+    #             'id': programa.id,
+    #             'nombre': programa.nombre,
+    #             'descripcion': programa.descripcion,
+    #         })
+
+    #     # Devolver respuesta JSON
+    #     return json.dumps(data)
     
-    # @http.route('/programas/<model("gestion.alumnos.programa"):programa_id>', auth="public")
-    # def tracking(self, programa_id):
-    #     return http.request.render({
-    #         "programa":programa_id
-    #     })
-
-    # @http.route("/check_method_get", auth='none',method=['GET'])
-    # def check_method_get(self,**values):
-    #     output = {
-    #         'results':{
-    #             'code':200,
-    #             'message':'OK'
-    #         }
-    #     }
-    #     return json.dumps(output['results'])
-    
-
-
-    # @http.route('/programas/<int:programa_id>', auth='public', method=['GET'])
-    # def get_alumnos_inscritos(self, programa_id):
-    #     programa = http.request.env['gestion.alumnos.programa']
-    #     data = json.loads(programa)
-    #     # programas = programa.sudo().search([('programa_id', '=', programa_id)])
-    #     # programas_json = {
-    #     #     'programas' : programas
-    #     # }
-    #     return json.dumps(programa)}
-
-    # @http.route('/programas', auth='public', method=['GET'])
-    # def get_alumnos_inscritos(self):
-    #     programa = http.request.env['gestion.alumnos.programa'].sudo().search([])
-    #     return programa
-
-    @http.route('/api/programas', auth='public')
-    def get_programas(self):
+    @http.route('/api/inscriptos/programa/<int:programa_id>', auth='public')
+    def get_programas(self, programa_id):
         # Obtener registros del modelo 'gestion.alumnos.programa'
-        programas = http.request.env['gestion.alumnos.programa'].search([])
+        programas = http.request.env['gestion.alumnos.inscripcion'].search([
+            ('programa_id', '=', programa_id)
+        ])
 
         # Convertir registros a formato JSON
         data = []
         for programa in programas:
             data.append({
                 'id': programa.id,
-                'nombre': programa.nombre,
-                'descripcion': programa.descripcion,
+                'alumno_id': programa.alumno_id.id,
+                'programa_id': programa.programa_id.id,
             })
 
         # Devolver respuesta JSON
         return json.dumps(data)
+    
