1290ca338e49238126254cbcaabbea6f9b6f3438
juanjricci <jr.juanricci@gmail.com>
2024-05-14 11:03:19 -0300

    added a validation function for the field telefono to be only digits

--- custom_addons/gestion_alumnos/models/gestion_alumnos_models.py
+++ custom_addons/gestion_alumnos/models/gestion_alumnos_models.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 # Modelo Alumno
 class Alumno(models.Model):
@@ -10,9 +11,16 @@ class Alumno(models.Model):
     fecha_de_nacimiento = fields.Date("Fecha de Nacimiento", required=True)
     legajo = fields.Integer("N° de Legajo")
     email = fields.Char("Email")
-    telefono = fields.Float("Telefono", size=15, digits=(15, 0))
+    telefono = fields.Char("Telefono")
+    # telefono = fields.Float("Telefono", size=15, digits=(15, 0))
     direccion = fields.Char("Direccion")
     pais = fields.Char("Pais")
+
+    @api.constrains('telefono')
+    def _check_phone_number(self):
+        for record in self:
+            if record.telefono and not record.telefono.isdigit():
+                raise ValidationError("El telefono debe contener solo digitos.")
 
 # Modelo Programa
 class Programa(models.Model):
